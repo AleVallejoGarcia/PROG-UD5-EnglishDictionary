7a84871b12e9b709adbb167439e3298249370901
AleVallejoGarcia <usr2075@salesianos-lacuesta.net>
2023-05-16 16:49:22 +0100

    feat:(App) Se lleva a cabo la integración de un menu en la clase ejecutable

--- EnglishDictionary/src/App.java
+++ EnglishDictionary/src/App.java
@@ -1,5 +1,55 @@
+import net.salesianos.duolingo.utilities.*;
+
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Dictionary dictionary = new Dictionary();
+        final Scanner KEYBOARD = new Scanner(System.in);
+        boolean salir = false;
+        String word;
+
+        while (!salir) {
+            int opcion;
+            System.out.println("1. Añadir palabra");
+            System.out.println("2. Eliminar palabra");
+            System.out.println("3. Existe palabra");
+            System.out.println("4. Mostrar iniciales disponibles");
+            System.out.println("5. Ver palabras por inicial");
+            System.out.println("6. Cerrar programa");
+            opcion = KEYBOARD.nextInt();
+            switch (opcion) {
+                case 1:
+                    KEYBOARD.nextLine();
+                    System.out.println("Escriba la palabra que desa añadir");
+                    word = KEYBOARD.nextLine().toLowerCase();
+                    word = word.replace(" ", "");
+                    dictionary.addWord(word);
+                    break;
+                case 2:
+                     KEYBOARD.nextLine();
+                    System.out.println("Escriba la palabra que desa eliminar");
+                    word = KEYBOARD.nextLine().toLowerCase();
+                    word = word.replace(" ", "");
+                    dictionary.deleteWord(word);
+                    break;
+                case 3:
+                    KEYBOARD.nextLine();
+                    System.out.println("Escriba la palabra que desa comprobar");
+                    word = KEYBOARD.nextLine().toLowerCase();
+                    word = word.replace(" ", "");
+                    dictionary.existWord(word);
+                    break;
+                case 4:
+                    dictionary.showInitials();
+                    break;
+                case 5:
+                    dictionary.showWords();
+                    break;
+                case 6:
+                    salir = true;
+                    break;
+            }
+        }
     }
 }
